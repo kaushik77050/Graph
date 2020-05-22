383a10f490207db61b37ade401c88ddd01664a7d
Kaushik Agrawal <46103076+kaushik77050@users.noreply.github.com>
2020-05-22 15:38:54 +0530

    Add files via upload

--- chart.js
+++ chart.js
@@ -1,33 +1,66 @@
-function myFunction() {
-  var x = document.getElementById("dropdown").value;
-  //x = "'" + x + "'";
-  document.getElementById("demo").innerHTML = "You selected: " + x;
+Chart.defaults.global.defaultFontFamily = 'Roboto';
+Chart.defaults.global.defaultFontColor = '#333';
+//var x;
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: x,
-
-      // The data for our dataset
-      data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45]
-          }]
-      },
-
-      // Configuration options go here
-      options: {
-        scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
-      }
-  });
+function myFunction(){
+  //  var x = document.getElementById("dropdown").value;
+  //document.getElementById("demo").innerHTML = "You selected: " + x;
+  // Request data using D3
+  d3.csv('https://s3-us-west-2.amazonaws.com/s.cdpn.io/2814973/atp_wta.csv').then(makeChart);
 }
+
+//document.getElementById("demo").innerHTML = "You selected: " + x;
+const changeHiddenInput = (objDropDown) =>{
+  console.log(objDropDown.value);
+}
+
+function makeChart(players) {
+  
+  // players is an array of objects where each object is something like:
+  // {
+  //   "Name": "Steffi Graf",
+  //   "Weeks": "377",
+  //   "Gender": "Female"
+  // }
+
+
+  var playerLabels = players.map(function(d) {return d.Name});
+  var weeksData = players.map(function(d) {return +d.Weeks});
+  var playerColors = players.map(function(d) {return d.Gender === 'Female' ? '#F15F36' : '#19A0AA';});
+
+
+  var x = document.getElementById("dropdown").value;
+  console.log(x);
+  var chart = new Chart('chart', {
+    type: x,
+    options: {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: 'Weeks at No.1',
+              fontSize: 16
+            }
+          }
+        ]
+      }
+    },
+    data: {
+      labels: playerLabels,
+      datasets: [
+        {
+          data: weeksData,
+          backgroundColor: playerColors
+        }
+      ]
+    }
+  })
+}
+
+
+
